b12f92e37ca53fbf28e7a3a5dc0c79df4dd001ad
Carlos Daniel <carloscggames@gmail.com>
2025-06-25 20:13:01 -0300

    feat: level 01 - solve exercise 01

---
+++ exercicio-01/01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Printf("x = %d | y = %s | z = %t\n", x, y, z)
+	fmt.Println("x =", x)
+	fmt.Println("y =", y)
+	fmt.Println("z =", z)
+}
