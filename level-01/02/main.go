b4f91ea8a3b5e8c40dd822e33c522c1286144c41
Carlos Daniel <carloscggames@gmail.com>
2025-06-25 20:31:22 -0300

    feat: level 01 - solve exercise 02

---
+++ level-01/02/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println("x = ", x) // 0
+	fmt.Println("y = ", y) // vazio
+	fmt.Println("z = ", z) // false
+
+	// Solução com Print Format:
+	fmt.Printf("x = %d | y = %s | z = %t\n", x, y, z)
+}
