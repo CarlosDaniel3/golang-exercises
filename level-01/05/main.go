8919c47aad6b06920816da5e11fd2421a1217337
Carlos Daniel <carloscggames@gmail.com>
2025-06-25 23:39:25 -0300

    feat: level 01 - solve exercise 05

---
+++ level-01/05/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type myType int
+var x myType
+var y int
+
+func main() {
+	// Primeira Parte
+	fmt.Printf("x = %v\n", x)
+	fmt.Printf("O tipo de x é %T\n", x)
+	x = 42
+	fmt.Printf("Agora, x vale %v\n", x)
+
+	fmt.Println("----------------------")
+
+	// Segunda Parte
+	y = int(x)
+	fmt.Printf("y = %v\n", y)
+	fmt.Printf("O Tipo de y é %T\n", y)
+}
